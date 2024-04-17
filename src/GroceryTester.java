7083563969d2592ef58f4799336671ef778c1716
DavideMzz9 <104537978+DavideMzz9@users.noreply.github.com>
2024-04-17 12:28:11 -0500

    Grocery store

--- src/GroceryTester.java
+++ src/GroceryTester.java
@@ -127,36 +127,7 @@ public class GroceryTester {
     }
 
 
-//        Undergrad Davide = new Undergrad("Mazzucco", "Davide", 7628, 12, 3, 2002, Undergrad.Level.JUNIOR);
-//        Graduate Angelo = new Graduate("Mazzucco", "Angelo", 8756, 20, 11, 2000);
-//
-//        Davide.startGroceryList(Marianos);
-//        Angelo.startGroceryList(WholeFoods);
-//
-//        GroceryItemOrder order1 = new GroceryItemOrder(item1, 4);
-//        GroceryItemOrder order2 = new GroceryItemOrder(item3, 2);
-//        GroceryItemOrder order3 = new GroceryItemOrder(item5, 1);
-//        GroceryItemOrder order4 = new GroceryItemOrder(item7, 5);
-//
-//
-//        GroceryItemOrder order22 = new GroceryItemOrder(item6, 3);
-//        GroceryItemOrder order33 = new GroceryItemOrder(item4, 2);
-//        GroceryItemOrder order44 = new GroceryItemOrder(item2, 4);
-//
-//
-//        Davide.getGroceryList().addItemOrder(order1);
-//        Davide.getGroceryList().addItemOrder(order2);
-//        Davide.getGroceryList().addItemOrder(order3);
-//        Davide.getGroceryList().addItemOrder(order4);
-//        Angelo.getGroceryList().addItemOrder(order1);
-//        Angelo.getGroceryList().addItemOrder(order1);
-//        Angelo.getGroceryList().addItemOrder(order22);
-//        Angelo.getGroceryList().addItemOrder(order33);
-//        Angelo.getGroceryList().addItemOrder(order44);
-//
-//
-//        Davide.displayGroceryListTotal();
-//        Angelo.displayGroceryListTotal();
+
 
 
 
